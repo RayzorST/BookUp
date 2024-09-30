d7d14c391803079326bd5a039b147db5027bfc64
Mabuuka <sgwork987@gmail.com>
2024-09-30 14:34:25 +0300

    searching fragment
    Searching fragment works now, but it doesn't work with database (coming soon)

--- app/src/main/java/com/example/bookup/SearchFragment.kt
+++ app/src/main/java/com/example/bookup/SearchFragment.kt
@@ -5,13 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import androidx.constraintlayout.helper.widget.Carousel.Adapter
+import com.example.bookup.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val book = arrayOf("first", "second")
+
+        val bookAdapter: ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            book
+        )
+
+        binding.booksList.adapter = bookAdapter
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                if (book.contains(query)) {
+                    bookAdapter.filter.filter(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                bookAdapter.filter.filter(newText)
+                return false
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
