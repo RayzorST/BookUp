b43577b1457f1daed55ec8175810db55a3aecc6c
Mabuuka <sgwork987@gmail.com>
2024-10-13 12:51:19 +0300

    themes change

--- app/src/main/java/com/example/bookup/SettingsFragment.kt
+++ app/src/main/java/com/example/bookup/SettingsFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        // Найти кнопки по их ID
+        val darkThemeButton = view.findViewById<Button>(R.id.darkTheme)
+        val lightThemeButton = view.findViewById<Button>(R.id.lightTheme)
+
+        // Обработчик нажатия для Dark Theme
+        darkThemeButton.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        // Обработчик нажатия для Light Theme
+        lightThemeButton.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        return view
     }
+
+
 
     companion object {
         @JvmStatic
